50119569cce577d3d250afb389531ca17bd21a06
Aymen Al-Ashwal <a91913@gmail.com>
2019-02-03 22:13:34 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="232 Project"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
